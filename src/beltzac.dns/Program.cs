507b4d28fc898e3c304d173e6d09c28c77ad687e
Alexandre Beltzac <ext_alexandre.beltzac@tcp.com.br>
2022-04-11 15:08:07 -0300

    Console app

---
+++ src/beltzac.dns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace beltzac.dns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
